99a487af217f5e3f9be524f12cb64eeb0b42900a
Nekibe-Bytyqi <nekibebytyqi@gmail.com>
2025-02-02 00:55:30 -0800

    shtimi i action te form ne html ndryshim nga username ne email modifikim te css dhe ndryhsimi nga username ne email te validation-login.js

--- validation-login.js
+++ validation-login.js
@@ -2,30 +2,27 @@ document.getElementById('form').addEventListener('submit', (e) => {
     e.preventDefault();
     let hasErrors = false;
 
-   
     document.querySelectorAll('.error-message').forEach(el => el.innerText = '');
 
-   
-    const username = document.getElementById('username').value.trim();
+    const email = document.getElementById('email').value.trim(); 
     const password = document.getElementById('password').value;
 
-   
-    if (!username) {
-        document.getElementById('username-error').innerText = 'Username is required.';
+    if (!email) {
+        document.getElementById('email-error').innerText = 'Email is required.';
         hasErrors = true;
     }
 
-   
     if (!password) {
         document.getElementById('password-error').innerText = 'Password is required.';
         hasErrors = true;
     }
+
     if (password.length < 8) {
         document.getElementById('password-error').innerText = 'Password must be at least 8 characters long.';
         hasErrors = true;
     }
+
     if (!hasErrors) {
-        alert("Login successful!");
-        form.submit();
+        document.getElementById('form').submit();
     }
 });
