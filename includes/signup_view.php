94eacacd12cbc2ad44b57bbcbc2f74ccce39f1a2
Nekibe-Bytyqi <nekibebytyqi@gmail.com>
2025-02-01 23:12:11 -0800

    permisimi i paraqitjes se erroreve

--- includes/signup_view.php
+++ includes/signup_view.php
@@ -8,7 +8,7 @@ function check_signup_errors(){
 
         echo "<br>";
         foreach($errors as $error){
-            echo '<p class ="form-error">' . $error . '</p>';
+            echo '<p class="form-error" style="color: red; font-weight: bold; margin-bottom: 10px; position: absolute; top: 0; left: 0; width: 100%; text-align: center; margin-top:100px;">' . $error . '</p>';
         }
 
         unset($_SESSION["error_signup"]);
